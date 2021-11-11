76804ae6f465c62f8065f335a0c54ea92e6e6661
rgiusti <rgiusti@alabamasolutions.com>
2021-11-10 22:34:53 -0300

    seccion about con rombos parallax.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,10 +7,24 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   @Input() svgcolor: string = '';
-
+  lastScroll = 0;
+  @ViewChild('body', {static: true}) body:any;
   constructor() { }
 
   ngOnInit(): void {
+    var count = 0;    
+    document.onscroll = (e) => {
+      let actualScroll = window.pageYOffset || document.documentElement.scrollTop;      
+      if(actualScroll < this.lastScroll){ // scrolled up.
+        if (count > 1){
+          count--;
+        }
+      } else if (actualScroll > this.lastScroll){ // scrolled down
+        if(count < 255){
+          count++;
+        }
+      }
+    }
   }
 
 }
